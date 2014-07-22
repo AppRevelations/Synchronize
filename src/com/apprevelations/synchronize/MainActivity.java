5692a584490d92f5cfa757a4ffc98097067b3bde
Lakshay Chhikara <lakshay.chhikara09@gmail.com>
2014-07-22 20:34:00 +0530

    ActionBar buttons added but they dont do anything yet

--- src/com/apprevelations/synchronize/MainActivity.java
+++ src/com/apprevelations/synchronize/MainActivity.java
@@ -24,6 +24,8 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.util.SparseArray;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
@@ -164,5 +166,28 @@ public class MainActivity extends ActionBarActivity implements BrowseFragment.On
         	
         }
 	}
+	
+	@Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        switch(id){
+        	case R.id.action_settings :
+        		return true;
+        		
+        	case R.id.action_send_all : 
+        		return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
 }
